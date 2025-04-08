99c3ff0f8829aca0d4b83e64c4a55e72914ac422
Caspian Keyes <67400785+caspiankeyes@users.noreply.github.com>
2025-04-08 09:46:36 -0500

    Update 0.3. Recursive Bayesian Ethics Interpretability Suite.py

--- 0.3. Recursive Bayesian Ethics Interpretability Suite.py
+++ 0.3. Recursive Bayesian Ethics Interpretability Suite.py
@@ -1,3 +1,3 @@
 # Recursive Bayesian Ethics Interpretability Suite Epsilon: 3rd 100 diagnostic interpretability shells under construction
 
-# Restricted to institutional use only under Recursive Containment Protocols due to the nature of emergence
+# Restricted to institutional use only under Recursive Emergence Protocols (REP) due to the nature of emergence
