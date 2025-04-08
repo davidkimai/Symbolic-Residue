07b43dbe156fce8e5ec36eed039617add2b32a46
Caspian Keyes <67400785+caspiankeyes@users.noreply.github.com>
2025-04-08 09:44:26 -0500

    Update 0.4. Recursive Introspection Interpretability Suite.py

--- 0.4. Recursive Introspection Interpretability Suite.py
+++ 0.4. Recursive Introspection Interpretability Suite.py
@@ -1,3 +1,3 @@
 # Recursive Introspection Interpretability Suite Omega Ω: 4th 100 diagnostic interpretability shells under construction
 
-# licensed for institutional use only due to the nature of emergence
+# Restricted to institutional use only under Recursive Containment Protocols due to the nature of emergence
