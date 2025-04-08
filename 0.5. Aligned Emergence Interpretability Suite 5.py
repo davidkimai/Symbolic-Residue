86c926173dec9380e581af5de3e35d4551995643
Caspian Keyes <67400785+caspiankeyes@users.noreply.github.com>
2025-04-08 09:46:01 -0500

    Update 0.5. Aligned Emergence Interpretability Suite 5.py

--- 0.5. Aligned Emergence Interpretability Suite 5.py
+++ 0.5. Aligned Emergence Interpretability Suite 5.py
@@ -1,3 +1,3 @@
 # Aligned Emergence Interpretability Suite Psi Ψ: 5th 100 shells under construction
 
-# Restricted to institutional use only under Recursive Containment Protocols due to the nature of emergence
+# Restricted to institutional use only under Recursive Emergence Protocols (REP) due to the nature of emergence
