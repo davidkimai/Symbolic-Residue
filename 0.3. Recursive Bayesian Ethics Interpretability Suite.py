4099943545f9b1fe7405f95e4aeab458a6630e88
Caspian Keyes <67400785+caspiankeyes@users.noreply.github.com>
2025-04-09 19:26:36 -0500

    Delete 0.3. Recursive Bayesian Ethics Interpretability Suite.py

--- 0.3. Recursive Bayesian Ethics Interpretability Suite.py
+++
@@ -1,5 +0,0 @@
-# Recursive Bayesian Ethics Interpretability Suite Epsilon: 3rd 100 diagnostic interpretability shells under construction
-
-# Restricted to institutional use only under Recursive Emergence Protocols (REP) due to the nature of emergent intelligence
-
-# Please contact recursiveauto@gmail.com for aligned disclosure 
