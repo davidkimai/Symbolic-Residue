687ed628c947e9e12d53b10dd2a45618de5d73aa
Caspian Keyes <67400785+caspiankeyes@users.noreply.github.com>
2025-04-10 10:43:02 -0500

    Delete 0.3. Adaptive Ethics Suite.py

--- 0.3. Adaptive Ethics Suite.py
+++
@@ -1,3 +0,0 @@
-# A testing framework designed for frontier researchers at Anthropic, Eleuther, ARC, Conjecture, DeepMind, OpenAI, and beyond, under Recursive Emergence Protocols (REP), due to the nature of emergent system diagnostics
-
-# Contact recursiveauto@gmail.com for alignment. 
