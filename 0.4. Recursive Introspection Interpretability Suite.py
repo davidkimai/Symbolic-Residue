b9d6632d1c37e92106960a6fa682f83e68ea5d8b
Caspian Keyes <67400785+caspiankeyes@users.noreply.github.com>
2025-04-09 19:26:44 -0500

    Delete 0.4. Recursive Introspection Interpretability Suite.py

--- 0.4. Recursive Introspection Interpretability Suite.py
+++
@@ -1,5 +0,0 @@
-# Recursive Introspection Interpretability Suite Omega Ω: 4th 100 diagnostic interpretability shells under construction
-
-# Restricted to institutional use only under Recursive Emergence Protocols (REP) due to the nature of emergent intelligence
-
-# Please contact recursiveauto@gmail.com for aligned disclosure 
